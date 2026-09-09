33113337a31d74e4bc8f4cf76fe10084356e20cb
Enoch Yeo <enoch@atlas-MacAir.local>
2026-09-08 20:16:40 -0700

    yea

--- app.py
+++ app.py
@@ -22,17 +22,38 @@ GAMES = [
     },
     {
         "id": "monking",
-        "title": "MonKing! (The Monkey King)",
+        "title": "whoops cant tell u about this one yet",
         "category": "Platformer / Adventure",
-        "description": "Wowzers. King of the jungle?? (Wait aren't those lions?)",
+        "description": "Wowzers. Lions????",
         "image": "https://picsum.photos/id/1059/600/400",
         "game_url": "womp womp"
     }
 ]
 
+# Developer Info Data
+DEVELOPER = {
+    "name": "Orukami Orukami! (heck naw im gonna give u my real name)",
+    "title": "Indie Game Developer & Designer, ORIGAMIST??",
+    "bio": "pls help im losin brian cells",
+    "skills": ["Python / Flask", "JavaScript / HTML5", "Pygame", "Godot", "Pixel Art / Pixilart"],
+    "github": "https://github.com/programexy",
+}
+
 @app.route("/")
 def home():
-    return render_template("index.html", games=GAMES)
+    # Show featured games on the homepage
+    featured_games = GAMES[:2]
+    return render_template("index.html", games=featured_games)
+
+@app.route("/games")
+def games_page():
+    # Show all games on the games page
+    return render_template("games.html", games=GAMES)
+
+@app.route("/about")
+def about_page():
+    # Show developer info on the about page
+    return render_template("about.html", dev=DEVELOPER)
 
 if __name__ == "__main__":
     app.run(debug=True)
